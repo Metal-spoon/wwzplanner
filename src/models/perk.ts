6c4372d27357b5a1e40a946c7cea1a3cb53178b7
Metal spoon <9281807+Metal-spoon@users.noreply.github.com>
2022-08-09 00:34:58 +0200

    changed column into computed property removed uneccesary props in url share component removed column data from json

--- src/models/perk.ts
+++ src/models/perk.ts
@@ -2,10 +2,21 @@ export class perk {
   Name: string = "";
   Description: string = "";
   teamWide: boolean = false;
-  column: number = 0;
   icon: string = "";
   level: number = 0;
   isBase: boolean = false;
   prestige: number = 0;
   selected: boolean = false;
+
+  get column(): number {
+    let column = 0;
+    if (this.isBase) {
+      column = this.prestige > 0 ? (this.prestige - 1) * 3 + (this.prestige - 1) : ((this.level / 10) * 3) + (this.level / 10)
+    } else {
+      const offset = Math.ceil(this.level / 10) - 1
+      column = Math.ceil((this.level - offset)/3) 
+      column = column + offset
+    }
+    return column;
+  }
 }
