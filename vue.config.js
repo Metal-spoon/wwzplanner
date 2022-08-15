f1dcb4c312ee666cb0e79362d3c5142ed634627c
Metal spoon <9281807+Metal-spoon@users.noreply.github.com>
2022-08-15 12:07:31 +0200

    Changed config to properly include 404.html when building

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/wwzplanner/" : "/",
+  pages: {
+    index: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: 'index.html'
+    },
+    404: {
+      entry: 'src/main.ts',
+      template: 'public/index.html',
+      filename: '404.html'
+    }
+  },
   configureWebpack: {
     devtool: "source-map",
   },
